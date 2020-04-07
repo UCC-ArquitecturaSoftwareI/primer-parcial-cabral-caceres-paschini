ab38fdad1b8fe803238132e89e609952d6f87ffc
martin22ca <50664520+martin22ca@users.noreply.github.com>
2020-04-07 19:44:44 +0000

    Deploying to gh-pages from  - 2eff9cea2a6eacd774efafe0eede054bad5fab9f 🚀

--- raylibTemplate.js.orig.js
+++ raylibTemplate.js.orig.js
@@ -194,7 +194,7 @@ Module['FS_createPath']('/', 'resources', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 10693, "filename": "/resources/shell.html"}, {"start": 10693, "audio": 0, "end": 12650, "filename": "/resources/ship.png"}, {"start": 12650, "audio": 1, "end": 2351570, "filename": "/resources/Cyberpunk Moonlight Sonata.mp3"}], "remote_package_size": 2351570, "package_uuid": "37752733-34ee-4d90-b1a2-93dc67da203c"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 10693, "filename": "/resources/shell.html"}, {"start": 10693, "audio": 0, "end": 12650, "filename": "/resources/ship.png"}, {"start": 12650, "audio": 1, "end": 2351570, "filename": "/resources/Cyberpunk Moonlight Sonata.mp3"}], "remote_package_size": 2351570, "package_uuid": "65b7b45a-adb6-403e-8b35-288354414127"});
 
 })();
 
