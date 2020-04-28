805fe005376c1a49ab9f36b9a161af94600de1fd
cata99 <catupaschini@hotmail.com>
2020-04-28 11:22:22 -0300

    Clase Padre (Darth vader)

---
+++ clases/Padre/padre.h
@@ -0,0 +1,33 @@
+//
+// Created by Catalina on 28/04/2020.
+//
+
+#ifndef PRIMER_PARCIAL_CABRAL_CACERES_PASCHINI_PADRE_H
+#define PRIMER_PARCIAL_CABRAL_CACERES_PASCHINI_PADRE_H
+
+#include <raylib.h>
+#include <string>
+#include <utility>
+#include <vector>
+class padre{
+protected:
+    std::string FilePath;
+    Vector2 Pos;
+    std:: vector<int> MaxColumn;
+    Rectangle Frame;
+public:
+
+    virtual void Move_x (float d) = 0;
+
+    virtual Vector2 &GetPos () = 0;
+
+    const std::string &getFilePath() const {
+        return FilePath;
+    }
+
+    const std::vector<int> &getMaxColumn() const {
+        return MaxColumn;
+    }
+};
+
+#endif //PRIMER_PARCIAL_CABRAL_CACERES_PASCHINI_PADRE_H
